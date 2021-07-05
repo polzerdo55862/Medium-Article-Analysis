a75ae7402c124456564b50f2df6a05a72e0cbf9c
Dominik Polzer <dominik.polzer@siemens.com>
2021-07-05 09:22:55 +0200

    added connection to SQLite database

--- helper_functions.py
+++ helper_functions.py
@@ -3,6 +3,7 @@ This module contains some rep
 """
 
 import sqlite3
+import sys
 
 def create_sqlite_connection(db_file):
     """ create a database connection to the SQLite database
@@ -14,19 +15,39 @@ def create_sqlite_connection(db_file):
     try:
         conn = sqlite3.connect(db_file)
         return conn
-    except Error as e:
-        print(e)
+    except:
+        print("Error:", sys.exc_info()[0])
 
     return conn
 
-def create_table(conn, create_table_sql):
+def create_table(cur, create_table_sql):
     """ create a table from the create_table_sql statement
-    :param conn: Connection object
+    :param cur: Defined cursor
     :param create_table_sql: a CREATE TABLE statement
     :return:
     """
     try:
-        c = conn.cursor()
-        c.execute(create_table_sql)
-    except Error as e:
-        print(e)
+        cur.execute(create_table_sql)
+    except:
+        print("Error:", sys.exc_info()[0])
+
+def drop_table(cur, table_to_drop):
+    """ drop a table
+    :param conn: Defined cursor
+    :param table_to_drop: specify the table name you want to drop
+    :return:
+    """
+
+    sql_drop_table = f"drop table {table_to_drop}"
+
+    try:
+        cur.execute(sql_drop_table)
+    except:
+        print("Error:", sys.exc_info()[0])
+
+def print_tables_in_database(cur):
+    try:
+        cur.execute("SELECT name FROM sqlite_master WHERE type='table';")
+        print(cur.fetchall())
+    except:
+        print("Error:", sys.exc_info()[0])
