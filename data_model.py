47c5596aff465b91f638213aea947d5a59517648
Dominik Polzer <dominik.polzer@siemens.com>
2021-07-08 20:45:01 +0200

    update helper_functions

--- data_model.py
+++ data_model.py
@@ -1,21 +1,10 @@
-# define sql statement to create table "stories" if not exist
+"""
+Compilation of SQL statements to create required data model
+"""
+
+# create the table with all basic information to the stories
 sql_create_table_stories = """
-                                CREATE TABLE IF NOT EXISTS test (
-
-                                    date_published INTEGER PRIMARY KEY,
-                                    title TEXT NOT NULL,
-                                    subtitle TEXT,
-                                    claps TEXT NOT NULL,
-                                    responses INTEGER,
-                                    author_url TEXT NOT NULL,
-                                    reading_time INTEGER
-
-                            );
-                            """
-
-# define sql statement to create table "stories" if not exist
-sql_create_table_stories = """
-                                CREATE TABLE IF NOT EXISTS test (
+                                CREATE TABLE IF NOT EXISTS stories (
 
                                     date_published INTEGER PRIMARY KEY,
                                     title TEXT NOT NULL,
--- helper_functions.py
+++ helper_functions.py
@@ -7,6 +7,7 @@ import sys
 import pandas as pd
 from bs4 import BeautifulSoup
 import requests
+import data_model
 
 ######################################################################################################################
 # Functions to interact with the SQLite database
@@ -20,143 +21,155 @@ class SQLiteConnection:
         :param db_file: database file
         :return: Connection object or None
         """
-        conn = None
+        self.conn = None
+        self.cur = None
+
         try:
             self.conn = sqlite3.connect(db_file)
             self.cur = self.conn.cursor()
         except:
             print("Error:", sys.exc_info()[0])
 
-    def create_table(cur, create_table_sql):
-        """ create a table from the create_table_sql statement
-        :param cur: Defined cursor
-        :param create_table_sql: a CREATE TABLE statement
-        :return:
-        """
+        # (if not exist) create table stories
         try:
-            cur.execute(create_table_sql)
+            self.cur.execute(data_model.sql_create_table_stories)
         except:
             print("Error:", sys.exc_info()[0])
 
-    def drop_table(cur, table_to_drop):
-        """ drop a table
-        :param conn: Defined cursor
-        :param table_to_drop: specify the table name you want to drop
-        :return:
+
+
+    def drop_table(self, table_to_drop):
+        """
+        drop a table
+            :param table_to_drop: String, Name of the table that should be dropped
         """
 
         sql_drop_table = f"drop table {table_to_drop}"
 
         try:
-            cur.execute(sql_drop_table)
+            self.cur.execute(sql_drop_table)
         except:
             print("Error:", sys.exc_info()[0])
 
-    def print_tables_in_database(cur):
+    def print_tables(self):
         try:
-            cur.execute("SELECT name FROM sqlite_master WHERE type='table';")
-            print(cur.fetchall())
+            self.cur.execute("SELECT name FROM sqlite_master WHERE type='table';")
+            print(self.cur.fetchall())
         except:
             print("Error:", sys.exc_info()[0])
 
-
-######################################################################################################################
-# Functions for web scraping
-######################################################################################################################
-
-# function to extract all the information to the stories given in the medium archive
-def extract_data(url, date_published):
-    page = requests.get(url)
-    soup = BeautifulSoup(page.text, 'html.parser')
-
-    stories = soup.find_all('div', class_='streamItem streamItem--postPreview js-streamItem')
-    stories_data = []
-
-    # find attributes author_url, reading_time, reading_time, responses, story_url for the stories which where published
-    # for the specified date
-
-    for story in stories:
-        each_story = []
-
-        author_box = story.find('div', class_='postMetaInline u-floatLeft u-sm-maxWidthFullWidth')
-        author_url = author_box.find('a')['href']
-
+    def execute(self, statement):
         try:
-            reading_time = author_box.find('span', class_='readingTime')['title']
+            self.cur.execute(statement)
+            print(self.cur.fetchall())
         except:
-            continue
-
-        title = story.find('h3').text if story.find('h3') else '-'
-        subtitle = story.find('h4').text if story.find('h4') else '-'
-
-        if story.find('button',
-                      class_='button button--chromeless u-baseColor--buttonNormal js-multirecommendCountButton u-disablePointerEvents'):
-
-            claps = story.find('button',
-                               class_='button button--chromeless u-baseColor--buttonNormal js-multirecommendCountButton u-disablePointerEvents').text
-
-        else:
-            claps = 0
-
-        if story.find('a', class_='button button--chromeless u-baseColor--buttonNormal'):
-
-            responses = story.find('a', class_='button button--chromeless u-baseColor--buttonNormal').text
-
-        else:
-            responses = '0 responses'
-
-        story_url = story.find('a', class_='button button--smaller button--chromeless u-baseColor--buttonNormal')[
-            'href']
-
-        # data cleaning
-        reading_time = reading_time.split()[0]
-        responses = responses.split()[0]
-
-        story_page = requests.get(story_url)
-        story_soup = BeautifulSoup(story_page.text, 'html.parser')
-
-        sections = story_soup.find_all('section')
-        story_paragraphs = []
-        section_titles = []
-
-        for section in sections:
-            paragraphs = section.find_all('p')
-            for paragraph in paragraphs:
-                story_paragraphs.append(paragraph.text)
-
-            subs = section.find_all('h1')
-            for sub in subs:
-                section_titles.append(sub.text)
-
-        number_sections = len(section_titles)
-        number_paragraphs = len(story_paragraphs)
-
-        each_story.append(date_published)
-        each_story.append(title)
-        each_story.append(subtitle)
-        each_story.append(claps)
-        each_story.append(responses)
-        each_story.append(author_url)
-        each_story.append(story_url)
-        each_story.append(reading_time)
-        each_story.append(number_sections)
-        each_story.append(section_titles)
-        each_story.append(number_paragraphs)
-        each_story.append(story_paragraphs)
-
-        stories_data.append(each_story)
-
-    # write data to data frame "df" and return df
-    columns = ['date_published', 'title', 'subtitle', 'claps', 'responses',
-               'author_url', 'story_url', 'reading_time',
-               'number_sections', 'section_titles',
-               'number_paragraphs', 'paragraphs']
-
-    df = pd.DataFrame(stories_data, columns=columns)
-    return df
-
+            print("Error:", sys.exc_info()[0])
+######################################################################################################################
+# functions for collection data
+######################################################################################################################
 
 class Story:
+    """
+    class to extract all the information to the stories given in the medium archive
+        :parameter year, month, day: date
+    """
+
+    def __init__(self, conn, year, month, day):
+
+        date_published = f'{month}/{day}/{year}'
+
+        # webpage for scraping
+        url = f'https://towardsdatascience.com/archive/{year}/{month}/{day}'
+
+        page = requests.get(url)
+        soup = BeautifulSoup(page.text, 'html.parser')
+
+        # find all entries to the stories found
+        stories = soup.find_all('div', class_='streamItem streamItem--postPreview js-streamItem')
+        stories_data = []
+
+        # extract data for attributes author_url, reading_time, reading_time, responses, story_url
+        for story in stories:
+            each_story = []
+
+            author_box = story.find('div', class_='postMetaInline u-floatLeft u-sm-maxWidthFullWidth')
+            author_url = author_box.find('a')['href']
+
+            try:
+                reading_time = author_box.find('span', class_='readingTime')['title']
+            except:
+                continue
+
+            title = story.find('h3').text if story.find('h3') else '-'
+            subtitle = story.find('h4').text if story.find('h4') else '-'
+
+            if story.find('button',
+                          class_='button button--chromeless u-baseColor--buttonNormal js-multirecommendCountButton u-disablePointerEvents'):
+
+                claps = story.find('button',
+                                   class_='button button--chromeless u-baseColor--buttonNormal js-multirecommendCountButton u-disablePointerEvents').text
+
+            else:
+                claps = 0
+
+            if story.find('a', class_='button button--chromeless u-baseColor--buttonNormal'):
+
+                responses = story.find('a', class_='button button--chromeless u-baseColor--buttonNormal').text
+
+            else:
+                responses = '0 responses'
+
+            story_url = story.find('a', class_='button button--smaller button--chromeless u-baseColor--buttonNormal')[
+                'href']
+
+            # data cleaning
+            reading_time = reading_time.split()[0]
+            responses = responses.split()[0]
+
+            story_page = requests.get(story_url)
+            story_soup = BeautifulSoup(story_page.text, 'html.parser')
+
+            sections = story_soup.find_all('section')
+            story_paragraphs = []
+            section_titles = []
+
+            for section in sections:
+                paragraphs = section.find_all('p')
+                for paragraph in paragraphs:
+                    story_paragraphs.append(paragraph.text)
+
+                subs = section.find_all('h1')
+                for sub in subs:
+                    section_titles.append(sub.text)
+
+            number_sections = len(section_titles)
+            number_paragraphs = len(story_paragraphs)
+
+            each_story.append(date_published)
+            each_story.append(title)
+            each_story.append(subtitle)
+            each_story.append(claps)
+            each_story.append(responses)
+            each_story.append(author_url)
+            each_story.append(story_url)
+            each_story.append(reading_time)
+            each_story.append(number_sections)
+            each_story.append(section_titles)
+            each_story.append(number_paragraphs)
+            each_story.append(story_paragraphs)
+
+            stories_data.append(each_story)
+
+        # write data to data frame "df" and return df
+        columns = ['date_published', 'title', 'subtitle', 'claps', 'responses',
+                   'author_url', 'story_url', 'reading_time',
+                   'number_sections', 'section_titles',
+                   'number_paragraphs', 'paragraphs']
+
+        self.stories = pd.DataFrame(stories_data, columns=columns)
+        #self.stories.to_sql("stories", conn, if_exists='append')
+
+class StoryContent:
     '''
         Class to collect and store all required data from a specific story
         :parameter
